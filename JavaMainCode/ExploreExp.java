b2e59681e9eee49519586ff4cfeea5e6150a2094
unknown <fengziyuan72@gmail.com>
2020-12-10 15:21:02 +0000

    0.0.2

--- JavaMainCode/ExploreExp.java
+++ JavaMainCode/ExploreExp.java
@@ -1,3 +1,5 @@
+import sun.awt.www.content.audio.x_aiff;
+
 /**
  * Challeng#5 ExploreExp
  *  
@@ -9,7 +11,26 @@
 public class ExploreExp
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        Double ex =0.0;
+        double x = 1.0;
+        System.out.printf("Math.E is %20.18f\n",Math.E);
+        System.out.printf("Math.exp(1.0)is %20.18f\n", Math.exp(1.0));
+        for (double n = 0; n < 21; n++){
+            ex += (factorial(Math.pow(x, n)))/(factorial(n));
+            System.out.println(ex);
+        }
+        System.out.println(10.0);
+    }
+
+
+    public static double factorial(Double n) {
+        double result = 1.0;
+        int conter = 2;
+        while (conter <= n) {
+            result = result * conter;
+            conter++;
+        }
+        return result;
     }
 }
